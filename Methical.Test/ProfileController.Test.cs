244caf706516c4555c4c680f5e8b661cc8471ac3
gmlunesa <gmlunesa@gmail.com>
2022-12-23 14:46:47 +0800

    Add unit tests

---
+++ Methical.Test/ProfileController.Test.cs
@@ -0,0 +1,37 @@
+﻿using Methical.Controllers;
+using Methical.Services;
+using Methical.Tests.Services.Mock;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methical.Tests
+{
+    public class ProfileControllerTest
+    {
+        private readonly ProfileController _controller;
+        private readonly IProfileService _service;
+
+        public ProfileControllerTest()
+        {
+            // Arrange
+            _service = new ProfileServiceMock();
+            _controller = new ProfileController(_service);
+        }
+
+        [Fact]
+        public async void Get_Generate_Profile_ReturnsValidResult()
+        {
+            bool IsStableDiffusion = false;
+
+            // Act
+            var result = await _controller.GenerateProfile(IsStableDiffusion);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+    }
+}
---
+++ Methical.Test/Services.Mock/ProfileServiceMock.cs
@@ -0,0 +1,26 @@
+﻿using Methical.Models;
+using Methical.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methical.Tests.Services.Mock
+{
+    internal class ProfileServiceMock : IProfileService
+    {
+        public async Task<Profile> GetProfileAsync(bool IsStableDiffusion)
+        {
+            Profile profile = new Profile
+            {
+                Name="Name",
+                Location="Location",
+                Bio="Bio",
+                PhotoBase64= "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII"
+            };
+
+            return profile;
+        }
+    }
+}
