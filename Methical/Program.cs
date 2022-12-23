f5bba9eef5aca33e85b087672bcee942450a151a
gmlunesa <gmlunesa@gmail.com>
2022-12-23 19:00:05 +0800

    Add Azure Key Vault support in production

--- Methical/Program.cs
+++ Methical/Program.cs
@@ -2,6 +2,10 @@ using Methical.Services;
 using Methical.Settings;
 using Microsoft.Extensions.Configuration;
 
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -29,6 +33,14 @@ if(builder.Environment.IsDevelopment())
     builder.Configuration.AddUserSecrets<Program>();
 }
 
+if(builder.Environment.IsProduction())
+{
+    Console.WriteLine("Production");
+    var secretClient = new SecretClient(
+        new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+        new DefaultAzureCredential());
+    builder.Configuration.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+}
 builder.Services.Configure<MLPlatformSettings>(builder.Configuration.GetSection("MLPlatform"));
 
 builder.Services.AddScoped<IProfileService, ProfileService>();
